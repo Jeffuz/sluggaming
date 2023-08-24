a1b0d9b0f982c699a49ed8c5329f3cc02401bdd6
Jeffuz <jeffzhang0049@gmail.com>
2023-08-23 17:39:03 -0700

    setup routes

--- sluggaming/src/App.js
+++ sluggaming/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NoPage from './pages/NoPage';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
