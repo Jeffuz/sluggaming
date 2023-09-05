7e56485e363fbe8df4c4456a0f83cd63fec5b634
Jeffuz <jeffzhang0049@gmail.com>
2023-09-05 16:38:55 -0700

    Uploading images and page creation

--- sluggaming/src/App.js
+++ sluggaming/src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NoPage from './pages/NoPage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+    
     <div className="App">
+      
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="*" element={<NoPage />} />
         </Routes>
---
+++ sluggaming/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='bg-[#00588F] w-full'>
+            <div className=''>
+                {/* Slug Gaming */}
+                <div>
+                    <>
+                    </>
+                    <div className='text-[#FFCC04]'>
+                        Slug Gaming
+                    </div>
+                </div>
+                {/* Contents */}
+                <div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
