092978cd235877efc154701570309cca43eee79a
Jeffuz <jeffzhang0049@gmail.com>
2023-09-22 15:48:21 -0700

    Complete Staff

--- sluggaming/src/components/Staff Page/StaffCard.jsx
+++ sluggaming/src/components/Staff Page/StaffCard.jsx
@@ -3,16 +3,31 @@ import React from 'react'
 const StaffCard = ({ name, role, location, image, username, pronouns, content }) => {
   return (
     <div className='font-Montserrat'>
-      <section id='front'>
-        <div className='border'>
-          <img src={require(`../../images/Staff Page/${image}`)} alt={name} loading="lazy" />
-          <div>{name}</div>
-          <div>{role}</div>
-          <div>{location}</div>
+      <section id='front' className='bg'>
+        <div className='shadow-lg rounded-[25px]'>
+          <div className='rounded-t-[25px]'>
+            {/* <img src={require(`../../images/Staff Page/${image}`)} alt={name} loading="lazy" /> */}
+            <img src={require(`../../images/Footer Component/SlugLogo.png`)} alt={name} loading="lazy" />
+          </div>
+          <div className='px-7 pt-7 pb-14 bg-[#00588F] rounded-b-[25px]'>
+            <div className='text-3xl text-[#FFCC04] mb-3'>
+              {name}
+            </div>
+            <div className='text-white text-2xl pt-1'>
+              {role}
+            </div>
+            <div className='text-white text-2xl flex items-center pt-1'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" className='mr-2'>
+                <path d="M12 12.75C13.6569 12.75 15 11.4069 15 9.75C15 8.09315 13.6569 6.75 12 6.75C10.3431 6.75 9 8.09315 9 9.75C9 11.4069 10.3431 12.75 12 12.75Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M19.5 9.75C19.5 16.5 12 21.75 12 21.75C12 21.75 4.5 16.5 4.5 9.75C4.5 7.76088 5.29018 5.85322 6.6967 4.4467C8.10322 3.04018 10.0109 2.25 12 2.25C13.9891 2.25 15.8968 3.04018 17.3033 4.4467C18.7098 5.85322 19.5 7.76088 19.5 9.75V9.75Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+              {location}
+            </div>
+          </div>
         </div>
       </section>
       <section id='back'>
-        <div className='bg-[#CCC] rounded-[20px] hidden'>
+        <div className='hidden'>
           <div>{username}</div>
           <div>{pronouns}</div>
           <div>{content}</div>
--- sluggaming/src/pages/Staff.js
+++ sluggaming/src/pages/Staff.js
@@ -17,14 +17,16 @@ const Staff = () => {
         <div className='min-[1100px]:px-[215px] px-[50px] text-[25px] pt-[30px] text-center'>
           Overseeing the backbone of Slug Gaming, the Executive Board arranges club meetings, sign-offs on official paperwork, and manages all the background tasks needed to keep the club running.
         </div>
-        <div>
-          {executiveBoardData.map((staffMember) => (
-            <StaffCard key={staffMember.id} {...staffMember} />
-          ))}
+        <div className='container mx-auto py-24 px-8'>
+          <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6'>
+            {executiveBoardData.map((staffMember) => (
+              <StaffCard key={staffMember.id} {...staffMember} />
+            ))}
+          </div>
         </div>
       </section>
       <section id='Esports Director'>
-        <div className='flex justify-center min-[1100px]:text-[60px] text-[50px] text-[#00588F]'>
+        <div className='flex justify-center min-[1100px]:text-[60px] text-[50px] text-[#00588F] pt-[50px]'>
           Esports Director
         </div>
         <div className='flex justify-center mt-[-10px] max-[1100px]:px-[50px] max-[1100px]:pt-[20px]'>
@@ -33,14 +35,16 @@ const Staff = () => {
         <div className='min-[1100px]:px-[215px] px-[50px] text-[25px] pt-[30px] text-center'>
           Working hard for our competitive teams, Esports Directors work with our variety of gaming teams to organize scrimmages, handle tournament schedules, and ensure success for our Esports scene.
         </div>
-        <div>
-          {esportsDirectorData.map((staffMember) => (
-            <StaffCard key={staffMember.id} {...staffMember} />
-          ))}
+        <div className='container mx-auto py-24 px-8'>
+          <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6'>
+            {esportsDirectorData.map((staffMember) => (
+              <StaffCard key={staffMember.id} {...staffMember} />
+            ))}
+          </div>
         </div>
       </section>
-      <section id='Team Leads'>
-        <div className='flex justify-center min-[1100px]:text-[60px] text-[50px] text-[#00588F]'>
+      <section id='Team Leads' className='pb-[50px]'>
+        <div className='flex justify-center min-[1100px]:text-[60px] text-[50px] text-[#00588F] pt-[50px]'>
           Team Leads
         </div>
         <div className='flex justify-center mt-[-10px] max-[1100px]:px-[50px] max-[1100px]:pt-[20px]'>
@@ -48,10 +52,12 @@ const Staff = () => {
         </div>
         <div className='min-[1100px]:px-[215px] px-[50px] text-[25px] pt-[30px] text-center'>
           Dedicated to fostering a thriving gaming community, Team Leads work to keep the community informed on the Esports scene through live streams, social media updates, and engaging social gaming nights.        </div>
-        <div>
-          {teamLeadsData.map((staffMember) => (
-            <StaffCard key={staffMember.id} {...staffMember} />
-          ))}
+        <div className='container mx-auto py-24 px-8'>
+          <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6'>
+            {teamLeadsData.map((staffMember) => (
+              <StaffCard key={staffMember.id} {...staffMember} />
+            ))}
+          </div>
         </div>
       </section>
     </div>
