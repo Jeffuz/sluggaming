d713ff41ec0eec32a9d049fa24761c22ba9635f8
Jeffuz <jeffzhang0049@gmail.com>
2023-09-29 18:43:13 -0700

    update responsiveness for home page

--- sluggaming/src/components/Home Panel/Event Panel.jsx
+++ sluggaming/src/components/Home Panel/Event Panel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Event_Panel = ({ Title, Quote, Name, Content, Image }) => {
     return (
-        <div className='min-[1305px]:pt-16 pt-8'>
+        <div className='min-[1305px]:pt-16 pt-8 min-[1305px]:mx-16 mx-8'>
             <div className='font-Montserrat flex flex-col'>
                 <div className='flex flex-col min-[1305px]:flex-row item' id='card'>
                     <div className='min-[1305px]:w-[50%] overflow-hidden' id='right-side'>
