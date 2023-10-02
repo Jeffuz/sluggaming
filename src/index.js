efedcb34112378c83eeefa8793b74e72ce6821ed
Jeffuz <jeffzhang0049@gmail.com>
2023-10-01 22:20:14 -0700

    hashrouting instead of browserrouting

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='sluggaming'>
+  <HashRouter>
+    {/* <BrowserRouter basename='sluggaming'> */}
     <App />
-  </BrowserRouter>
+    {/* </BrowserRouter> */}
+  </HashRouter>
 
 );
 
