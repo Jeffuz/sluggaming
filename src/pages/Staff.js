532ffdf6e87218919889efcdf524309aeef61e54
Jeffuz <jeffzhang0049@gmail.com>
2024-01-08 13:30:20 -0800

    Update Staff.js

--- src/pages/Staff.js
+++ src/pages/Staff.js
@@ -3,75 +3,64 @@ import StaffCard from '../components/Staff Page/StaffCard'
 import esportsDirectorData from '../data/Esports Director.json'
 import executiveBoardData from '../data/Executive Board.json'
 import teamLeadsData from '../data/Team Leads.json'
-import { motion } from 'framer-motion';
 
 const Staff = () => {
-  const containerVariants = {
-    hidden: { opacity: 0, y: 50 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: "easeInOut" } },
-  };
   return (
-    <motion.div
-      variants={containerVariants}
-      initial="hidden"
-      animate="visible"
-    >
-      <div className='pt-36 font-Montserrat'>
-        <section id='Executive Board'>
-          <div className='flex justify-center sm:text-5xl text-3xl text-[#00588F]'>
-            Executive Board
+    <div className='pt-36 font-Montserrat'>
+      <section id='Executive Board'>
+        <div className='flex justify-center sm:text-5xl text-3xl text-[#00588F]'>
+          Executive Board
+        </div>
+        <div className='flex justify-center max-[1100px]:px-[50px] pt-3'>
+          <img src={require('../images/Achivements Page/Yellow Line.png')} alt='Yellow Line' loading="lazy" />
+        </div>
+        <div className='container mx-auto px-8 sm:text-2xl text-lg pt-3 text-center'>
+          Overseeing the backbone of Slug Gaming, the Executive Board arranges club meetings, sign-offs on official paperwork, and manages all the background tasks needed to keep the club running.
+        </div>
+        <div className='container mx-auto p-8'>
+          <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6'>
+            {executiveBoardData.map((staffMember) => (
+              <StaffCard key={staffMember.id} {...staffMember} />
+            ))}
           </div>
-          <div className='flex justify-center max-[1100px]:px-[50px] pt-3'>
-            <img src={require('../images/Achivements Page/Yellow Line.png')} alt='Yellow Line' loading="lazy" />
+        </div>
+      </section>
+      <section id='Esports Director'>
+        <div className='flex justify-center sm:text-5xl text-3xl text-[#00588F]'>
+          Esports Director
+        </div>
+        <div className='flex justify-center max-[1100px]:px-[50px] pt-3'>
+          <img src={require('../images/Achivements Page/Yellow Line.png')} alt='Yellow Line' loading="lazy" />
+        </div>
+        <div className='container mx-auto px-8 sm:text-2xl text-lg pt-3 text-center'>
+          Working hard for our competitive teams, Esports Directors work with our variety of gaming teams to organize scrimmages, handle tournament schedules, and ensure success for our Esports scene.
+        </div>
+        <div className='container mx-auto p-8'>
+          <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6'>
+            {esportsDirectorData.map((staffMember) => (
+              <StaffCard key={staffMember.id} {...staffMember} />
+            ))}
           </div>
-          <div className='container mx-auto px-8 sm:text-2xl text-lg pt-3 text-center'>
-            Overseeing the backbone of Slug Gaming, the Executive Board arranges club meetings, sign-offs on official paperwork, and manages all the background tasks needed to keep the club running.
+        </div>
+      </section>
+      <section id='Team Leads' className='pb-[50px]'>
+        <div className='flex justify-center sm:text-5xl text-3xl text-[#00588F]'>
+          Team Leads
+        </div>
+        <div className='flex justify-center max-[1100px]:px-[50px] pt-3'>
+          <img src={require('../images/Achivements Page/Yellow Line.png')} alt='Yellow Line' loading="lazy" />
+        </div>
+        <div className='container mx-auto px-8 sm:text-2xl text-lg pt-3 text-center'>
+          Dedicated to fostering a thriving gaming community, Team Leads work to keep the community informed on the Esports scene through live streams, social media updates, and engaging social gaming nights.        </div>
+        <div className='container mx-auto p-8'>
+          <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6'>
+            {teamLeadsData.map((staffMember) => (
+              <StaffCard key={staffMember.id} {...staffMember} />
+            ))}
           </div>
-          <div className='container mx-auto p-8'>
-            <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6'>
-              {executiveBoardData.map((staffMember) => (
-                <StaffCard key={staffMember.id} {...staffMember} />
-              ))}
-            </div>
-          </div>
-        </section>
-        <section id='Esports Director'>
-          <div className='flex justify-center sm:text-5xl text-3xl text-[#00588F]'>
-            Esports Director
-          </div>
-          <div className='flex justify-center max-[1100px]:px-[50px] pt-3'>
-            <img src={require('../images/Achivements Page/Yellow Line.png')} alt='Yellow Line' loading="lazy" />
-          </div>
-          <div className='container mx-auto px-8 sm:text-2xl text-lg pt-3 text-center'>
-            Working hard for our competitive teams, Esports Directors work with our variety of gaming teams to organize scrimmages, handle tournament schedules, and ensure success for our Esports scene.
-          </div>
-          <div className='container mx-auto p-8'>
-            <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6'>
-              {esportsDirectorData.map((staffMember) => (
-                <StaffCard key={staffMember.id} {...staffMember} />
-              ))}
-            </div>
-          </div>
-        </section>
-        <section id='Team Leads' className='pb-[50px]'>
-          <div className='flex justify-center sm:text-5xl text-3xl text-[#00588F]'>
-            Team Leads
-          </div>
-          <div className='flex justify-center max-[1100px]:px-[50px] pt-3'>
-            <img src={require('../images/Achivements Page/Yellow Line.png')} alt='Yellow Line' loading="lazy" />
-          </div>
-          <div className='container mx-auto px-8 sm:text-2xl text-lg pt-3 text-center'>
-            Dedicated to fostering a thriving gaming community, Team Leads work to keep the community informed on the Esports scene through live streams, social media updates, and engaging social gaming nights.        </div>
-          <div className='container mx-auto p-8'>
-            <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6'>
-              {teamLeadsData.map((staffMember) => (
-                <StaffCard key={staffMember.id} {...staffMember} />
-              ))}
-            </div>
-          </div>
-        </section>
-      </div>
-    </motion.div>
+        </div>
+      </section>
+    </div>
   );
 };
 
