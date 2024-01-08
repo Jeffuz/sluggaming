1218827d00decfe74f57e01006e6783862dce537
Jeffuz <jeffzhang0049@gmail.com>
2024-01-08 13:26:23 -0800

    fix padding for responsiveness on staff page

--- src/pages/Staff.js
+++ src/pages/Staff.js
@@ -16,18 +16,18 @@ const Staff = () => {
       initial="hidden"
       animate="visible"
     >
-      <div className='pt-[150px] font-Montserrat'>
+      <div className='pt-36 font-Montserrat'>
         <section id='Executive Board'>
-          <div className='flex justify-center min-[1100px]:text-[60px] text-[50px] text-[#00588F]'>
+          <div className='flex justify-center sm:text-5xl text-3xl text-[#00588F]'>
             Executive Board
           </div>
-          <div className='flex justify-center mt-[-10px] max-[1100px]:px-[50px] max-[1100px]:pt-[20px]'>
+          <div className='flex justify-center max-[1100px]:px-[50px] pt-3'>
             <img src={require('../images/Achivements Page/Yellow Line.png')} alt='Yellow Line' loading="lazy" />
           </div>
-          <div className='min-[1100px]:px-[215px] px-[50px] text-[25px] pt-[30px] text-center'>
+          <div className='container mx-auto px-8 sm:text-2xl text-lg pt-3 text-center'>
             Overseeing the backbone of Slug Gaming, the Executive Board arranges club meetings, sign-offs on official paperwork, and manages all the background tasks needed to keep the club running.
           </div>
-          <div className='container mx-auto py-24 px-8'>
+          <div className='container mx-auto p-8'>
             <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6'>
               {executiveBoardData.map((staffMember) => (
                 <StaffCard key={staffMember.id} {...staffMember} />
@@ -36,16 +36,16 @@ const Staff = () => {
           </div>
         </section>
         <section id='Esports Director'>
-          <div className='flex justify-center min-[1100px]:text-[60px] text-[50px] text-[#00588F] pt-[50px]'>
+          <div className='flex justify-center sm:text-5xl text-3xl text-[#00588F]'>
             Esports Director
           </div>
-          <div className='flex justify-center mt-[-10px] max-[1100px]:px-[50px] max-[1100px]:pt-[20px]'>
+          <div className='flex justify-center max-[1100px]:px-[50px] pt-3'>
             <img src={require('../images/Achivements Page/Yellow Line.png')} alt='Yellow Line' loading="lazy" />
           </div>
-          <div className='min-[1100px]:px-[215px] px-[50px] text-[25px] pt-[30px] text-center'>
+          <div className='container mx-auto px-8 sm:text-2xl text-lg pt-3 text-center'>
             Working hard for our competitive teams, Esports Directors work with our variety of gaming teams to organize scrimmages, handle tournament schedules, and ensure success for our Esports scene.
           </div>
-          <div className='container mx-auto py-24 px-8'>
+          <div className='container mx-auto p-8'>
             <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6'>
               {esportsDirectorData.map((staffMember) => (
                 <StaffCard key={staffMember.id} {...staffMember} />
@@ -54,15 +54,15 @@ const Staff = () => {
           </div>
         </section>
         <section id='Team Leads' className='pb-[50px]'>
-          <div className='flex justify-center min-[1100px]:text-[60px] text-[50px] text-[#00588F] pt-[50px]'>
+          <div className='flex justify-center sm:text-5xl text-3xl text-[#00588F]'>
             Team Leads
           </div>
-          <div className='flex justify-center mt-[-10px] max-[1100px]:px-[50px] max-[1100px]:pt-[20px]'>
+          <div className='flex justify-center max-[1100px]:px-[50px] pt-3'>
             <img src={require('../images/Achivements Page/Yellow Line.png')} alt='Yellow Line' loading="lazy" />
           </div>
-          <div className='min-[1100px]:px-[215px] px-[50px] text-[25px] pt-[30px] text-center'>
+          <div className='container mx-auto px-8 sm:text-2xl text-lg pt-3 text-center'>
             Dedicated to fostering a thriving gaming community, Team Leads work to keep the community informed on the Esports scene through live streams, social media updates, and engaging social gaming nights.        </div>
-          <div className='container mx-auto py-24 px-8'>
+          <div className='container mx-auto p-8'>
             <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6'>
               {teamLeadsData.map((staffMember) => (
                 <StaffCard key={staffMember.id} {...staffMember} />
